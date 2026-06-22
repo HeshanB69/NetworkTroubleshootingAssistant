ea2dedde7f3283093c677f2b9bcbf28493ca1a77
Heshan Bimsara <heshanbimsar404@gmail.com>
2026-06-22 14:41:04 +0530

    Added gateway detection

--- connectivity.py
+++ connectivity.py
@@ -1,5 +1,6 @@
 import subprocess
 import socket
+import os
 
 def check_internet():
     try:
@@ -35,9 +36,36 @@ def check_dns():
 
     except Exception as e:
         return f"DNS Failed: {e}"
+    
+def get_gateway():
+    try:
+        output = os.popen("ipconfig").read()
+
+        lines = output.splitlines()
+
+        for i, line in enumerate(lines):
+            if "Default Gateway" in line:
+                gateway = line.split(":")[-1].strip()
+
+                if gateway and "." in gateway:
+                    return gateway
+
+                # Check next line for IPv4 gateway
+                if i + 1 < len(lines):
+                    next_line = lines[i + 1].strip()
+                    if "." in next_line:
+                        return next_line
+
+        return "Gateway Not Found"
+
+    except Exception as e:
+        return f"Error: {e}"    
+
+
 
 
 
 print("IP Address:", get_ip_address())
 print("Internet Status:", check_internet())
 print("DNS Status:", check_dns())
+print("Gateway:", get_gateway())
