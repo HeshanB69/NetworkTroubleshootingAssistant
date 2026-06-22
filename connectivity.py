79f3fad6ad235309bf26bcf1fbef156aad155967
Heshan Bimsara <heshanbimsar404@gmail.com>
2026-06-22 14:27:24 +0530

    Added IP address detection feature

---
+++ connectivity.py
@@ -0,0 +1,33 @@
+import subprocess
+import socket
+
+def check_internet():
+    try:
+        result = subprocess.run(
+            ["ping", "8.8.8.8", "-n", "1"],
+            capture_output=True,
+            text=True
+        )
+
+        if result.returncode == 0:
+            return "Internet Connected"
+
+        return "Internet Disconnected"
+
+    except Exception as e:
+        return f"Error: {e}"
+
+
+def get_ip_address():
+    try:
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+
+        return ip
+
+    except Exception as e:
+        return f"Error: {e}"
+
+
+print("IP Address:", get_ip_address())
+print("Internet Status:", check_internet())
